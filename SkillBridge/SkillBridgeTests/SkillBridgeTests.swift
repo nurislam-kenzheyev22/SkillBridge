5c491c2054c5876c97880f2dff7fd9ecf6b94092
Kenzheyev Nurislam <35447@iitu.edu.kz>
2026-01-05 04:44:24 +0500

    feat: Add Xcode project files and complete project structure

---
+++ SkillBridge/SkillBridgeTests/SkillBridgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkillBridgeTests.swift
+//  SkillBridgeTests
+//
+//  Created by Nurislam on 04.01.2026.
+//
+
+import Testing
+@testable import SkillBridge
+
+struct SkillBridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
